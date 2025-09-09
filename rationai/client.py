982ab1336585d749843ef3916b10bbae4d3395dd
JiriStipek <567776@muni.cz>
2025-09-09 15:53:02 +0200

    feat: change client

--- rationai/client.py
+++ rationai/client.py
@@ -1,30 +1,43 @@
 from functools import cached_property
 
+import numpy as np
 from aiohttp import ClientSession
 
 from rationai.resources.segmentation import AsyncNucleiSegmentation
 
 
-class AyncClient:
+class AsyncClient:
     def __init__(self, base_url: str) -> None:
-        self.session = ClientSession(
-            base_url=base_url, headers={"Content-Type": "application/octet-stream"}
+        self._base_url = base_url
+        self._session: ClientSession | None = None
+
+    async def __aenter__(self):
+        self._session = ClientSession(
+            base_url=self._base_url,
+            headers={"Content-Type": "application/octet-stream"},
         )
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        if self._session:
+            await self._session.close()
 
     @cached_property
     def nuclei_segmentation(self) -> AsyncNucleiSegmentation:
-        return AsyncNucleiSegmentation(self.session)
-
-
-import numpy as np
+        if not self._session:
+            raise RuntimeError(
+                "Client session not initialized. Use `async with AsyncClient(...)`."
+            )
+        return AsyncNucleiSegmentation(self._session)
 
 
 async def main():
-    client = AyncClient("http://127.0.0.1:8000")
-
-    x = await client.nuclei_segmentation(
-        input=np.zeros((256, 256, 3), dtype=np.uint8), model="lsp-detr"
-    )
+    async with AsyncClient("http://127.0.0.1:8000") as client:
+        result = await client.nuclei_segmentation(
+            input=np.zeros((256, 256, 3), dtype=np.uint8),
+            model="lsp-detr",
+        )
+        print(result)
 
 
 if __name__ == "__main__":
