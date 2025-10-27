954d97c9b1d4f3dafc4c65ff2402f48a9b8f5965
JiriStipek <567776@muni.cz>
2025-10-27 21:38:02 +0100

    feat: remove useless files and merge it into one

--- rationai/segmentation/types.py
+++ rationai/segmentation/types.py
@@ -1,6 +1,15 @@
 from typing import List, TypedDict
 
+import numpy as np
+from numpy.typing import NDArray
+
 
 class Result(TypedDict):
     polygons: List[List[List[float]]]
     embeddings: List[List[float]]
+
+
+class Tile(TypedDict):
+    data: NDArray[np.uint8]
+    x: int
+    y: int
