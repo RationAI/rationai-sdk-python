f7b63c204fbf826a09ad211295e26361a5733ca9
JiriStipek <567776@muni.cz>
2025-11-18 21:33:24 +0100

    feat: add new sync client

--- rationai/client.py
+++ rationai/client.py
@@ -1,7 +1,8 @@
 from aiohttp import ClientSession
+from requests import Session
 
-from rationai.resources.models import AsyncModels
-from rationai.resources.qc import AsyncQualityControl
+from rationai.resources.models import AsyncModels, SyncModels
+from rationai.resources.qc import AsyncQualityControl, SyncQualityControl
 
 
 class AsyncClient(ClientSession):
@@ -20,3 +21,22 @@ class AsyncClient(ClientSession):
     @property
     def qc(self) -> AsyncQualityControl:
         return AsyncQualityControl(self)
+
+
+class SyncClient(Session):
+    def __init__(
+        self,
+        base_url: str = "http://rayservice-model-serve-svc.rationai-jobs-ns.svc.cluster.local:8000",
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.base_url = base_url.rstrip("/")
+
+    @property
+    def models(self) -> SyncModels:
+        return SyncModels(self)
+
+    @property
+    def qc(self) -> SyncQualityControl:
+        return SyncQualityControl(self)
