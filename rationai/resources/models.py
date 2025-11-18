574602a1ccc0f16b071474c2b16de4f7240eafdf
JiriStipek <567776@muni.cz>
2025-11-18 21:33:48 +0100

    feat: add new syncmodels

--- rationai/resources/models.py
+++ rationai/resources/models.py
@@ -2,7 +2,7 @@ import numpy as np
 from aiohttp import ClientTimeout
 from numpy.typing import NDArray
 
-from rationai._resource import AsyncAPIResource
+from rationai._resource import AsyncAPIResource, SyncAPIResource
 
 
 class AsyncModels(AsyncAPIResource):
@@ -21,3 +21,19 @@ class AsyncModels(AsyncAPIResource):
             model, json={"input": image}, timeout=timeout
         ) as response:
             return await response.json()
+
+
+class SyncModels(SyncAPIResource):
+    def classify_image(
+        self, model: str, image: NDArray[np.uint8], timeout: float | None = None
+    ):
+        response = self._post(model, json={"input": image.tolist()}, timeout=timeout)
+        response.raise_for_status()
+        return response.json()
+
+    def embed_image(
+        self, model: str, image: NDArray[np.uint8], timeout: float | None = None
+    ):
+        response = self._post(model, json={"input": image.tolist()}, timeout=timeout)
+        response.raise_for_status()
+        return response.json()
