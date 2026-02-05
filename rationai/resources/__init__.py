159e110eaa597768c3a2788c9e1cb17e326969a9
JiriStipek <567776@muni.cz>
2026-02-05 17:19:05 +0100

    feat: add init file

---
+++ rationai/resources/__init__.py
@@ -0,0 +1,13 @@
+from rationai.resources.models import AsyncModels, Models
+from rationai.resources.qc import AsyncQualityControl, QualityControl
+from rationai.resources.slide import AsyncSlide, Slide
+
+
+__all__ = [
+    "AsyncModels",
+    "AsyncQualityControl",
+    "AsyncSlide",
+    "Models",
+    "QualityControl",
+    "Slide",
+]
